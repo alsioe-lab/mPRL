8081c86685eb3b01ebe9b6ebb5f042492b124a16
FreyaD13 <gi23004@bristol.ac.uk>
2025-10-23 10:32:30 +0100

    Add files via upload

---
+++ Update_Data.R
@@ -0,0 +1,252 @@
+####Update saved data
+
+# Reading in data from mPRL task
+# This code assumes that the number of columns in the data is 60
+rm(list=ls())
+library(tidyverse)
+
+n_col <- 60
+
+# Enter the link to the data folder here
+# DIR <- "C:/Users/sy22091/OneDrive - University of Bristol/Documents - grp-Alsiö lab-Computational models of cognition and motivation/GRIN2A BJA001 BJA002/"
+
+DIR <- file.path('C:/Users',
+                 'gi23004',
+                 'OneDrive - University of Bristol',
+                 'Documents',
+                 'mPRL',
+                 'Raw Data')
+
+# Check which file (currently just one)
+filenames <- list.files(DIR)
+
+# Pick a set of files to analyse
+LIST_OF_FILES <- filenames[grep(pattern = '_Combined.csv',
+                                x = filenames)]
+
+# Specify which subjects to include ...
+subjects <- c("fd_01_1",
+              "fd_01_2",
+              "fd_01_3",
+              "fd_01_4",
+              "fd_01_5",
+              "fd_01_6",
+              "fd_01_7",
+              "fd_01_8",
+              "fd_01_9",
+              "fd_01_10",
+              "fd_01_11",
+              "fd_01_12")
+
+# Troubleshooting
+
+for (FILE in LIST_OF_FILES) {
+  
+  # Create an empty data frame on the first iteration of the FILE loop
+  if (FILE == LIST_OF_FILES[1]) data <- data.frame()
+  
+  # Read in the data from FILE as lines
+  as_lines <- read_lines(file.path(DIR, FILE))
+  
+  # Identify relevant rows (indices) for various bits of information 
+  #fd: this finds which lines contain the phrase 'Subject Id'
+  pos_Subject_Id <- grep(pattern = TRUE,
+                         x = str_starts(string = as_lines,
+                                        pattern = 'Subject Id')
+  )
+  
+  # List the subjects
+  #first create a blank vector the same length as the number of times
+  #'subject id' appears
+  subjects_in_session <- vector(mode = "logical",
+                                length = length(pos_Subject_Id))
+  
+  # Writing this as a for loop to get the Subject IDs out
+  #fd: changed from johans 'as.numeric' because my subjects have fd in their id
+  for (i in 1:length(pos_Subject_Id))  {
+    subjects_in_session[i] <- as.character(
+      str_split(string = as_lines[pos_Subject_Id[i]],
+                pattern = ',')[[1]][2]
+    )
+  }
+  
+  
+  ###fd: Extracting information about session type and duration
+  pos_session_prot <- grep(pattern = TRUE,
+                           x = str_starts(string = as_lines,
+                                          pattern = 'Session Title')
+  )
+  protocols_in_day <- vector(mode = "logical",
+                             length = length(pos_session_prot))
+  for (i in 1:length(pos_session_prot))  {
+    protocols_in_day[i] <- as.character(
+      str_split(string = as_lines[pos_session_prot[i]],
+                pattern = ',')[[1]][2]
+    )
+  }
+  
+  #...same for incomplete sessions
+  pos_session_dur <- grep(pattern = TRUE,
+                          x = str_starts(string = as_lines,
+                                         pattern = 'Duration')
+  )
+  session_durations <- vector(mode = "logical",
+                              length = length(pos_session_dur))
+  for (i in 1:length(pos_session_dur))  {
+    session_durations[i] <- as.numeric(
+      str_split(string = as_lines[pos_session_dur[i]],
+                pattern = ',')[[1]][2]
+    )
+  }
+  
+  
+  #only continue with this file if at least one subject of interest is present
+  if (!sum(subjects %in% subjects_in_session)==0){
+    
+    ################
+    #fd: issue here, some alternative types of protocol dont have activity logs
+    #at end. need to work out a way to be consistent ie include all types of
+    #protocol start & finish pavlovian need a better way to work out end
+    
+    #find the points where one subject begins
+    pos_starts <- grep(pattern = TRUE,
+                       x = str_starts(string = as_lines,
+                                      pattern = 'Ref')
+    ) + 1
+    
+    #find the points where it ends
+    pos_ends <- grep(pattern = TRUE,
+                     x = str_starts(string = as_lines,
+                                    pattern = 'ACTIVITYLOG')
+    ) - 2
+    
+    ##fd: ask johan the most efficient way to do this, want to remove
+    #values from pos_starts based on protocols_in_day
+    #troubleshootig check;
+    if (!length(protocols_in_day)==length(session_durations)){
+      warning("PROTOCOLS IN DAY DOESNT MATCH SESSION DURATIONS")
+    }
+    
+    session_notes<-
+      data.frame(
+        protocols=protocols_in_day,
+        duration=session_durations,
+        pos_starts=pos_starts,
+        subjects_in_session=subjects_in_session,
+        pos_Subject_Id=pos_Subject_Id,
+        include=TRUE
+      )
+    
+    for (PROT in 1:dim(session_notes)[1]){
+      if ((!session_notes$protocols[PROT]=="PRL")|(session_notes$duration[PROT]<30000)){
+        session_notes$include[PROT]<-FALSE
+      }
+    }
+    
+    session_notes<-session_notes|>
+      filter(include==TRUE)
+    #returning it to a list, in future can pull data from df but cba now
+    pos_starts<-session_notes$pos_starts
+    subjects_in_session<-session_notes$subjects_in_session
+    pos_Subject_Id<-session_notes$pos_Subject_Id
+    
+    #subject<-1
+    #subject<-"fd_01_1"
+    
+    # This might need to be updated to cope with multiple runs/subject
+    #fd: quick fix, check if aborted early and skip if so
+    for (subject in subjects) {
+      
+      # # Error handling
+      #   #think this is causing entire loop to stop
+      # stopifnot(subject %in% subjects_in_session)
+      
+      #does the subject appear in this experiment?
+      if (subject %in% subjects_in_session){
+        
+        index <- match(subject, subjects_in_session)
+        
+        # Extract the date
+        date = str_split(string = as_lines[pos_Subject_Id - 16][index],
+                         pattern = ',')[[1]][2]
+        
+        # Extract the time
+        time = str_split(string = as_lines[pos_Subject_Id - 15][index],
+                         pattern = ',')[[1]][2]
+        
+        # Extract the box
+        box = str_split(string = as_lines[pos_Subject_Id - 14][index],
+                        pattern = ',')[[1]][2]
+        
+        # Extract the protocol fd: edited to make +3, not +2
+        day_table = str_split(string = as_lines[pos_Subject_Id + 3][index],
+                              pattern = ',')[[1]][2]
+        
+        # ~~~~~~~~~~~~~~~~~~~Sorting out the trials~~~~~~~~~~~~~~~~~~~~~
+        
+        # Extract numeric values from the session's trials
+        # This does not appear to work on the older sessions - check out why
+        trials_as_list <-
+          sapply(X = str_split(as_lines[pos_starts[index]:pos_ends[index]],
+                               pattern = ','),
+                 FUN = as.numeric)
+        
+        # Extract the numeric values for the list - note we ignore the start trial
+        for (j in 2:length(trials_as_list)) {
+          
+          # Set up empty data frame
+          if (j == 2) trials <- data.frame()
+          
+          # Add the line from the list to the data frame, if the format is correct
+          if (length(trials_as_list[[j]]) == n_col) {
+            trials <- rbind(trials, trials_as_list[[j]])
+          }
+          
+          # Update the names of the columns
+          if (j == length(trials_as_list)) {
+            names(trials) <- paste0("c", 1:dim(trials)[2])
+          }
+          
+        }
+        
+        # Update the trials data frame
+        trials$subj_id <- subject
+        trials$date <- date
+        trials$time <- time
+        trials$box <- box
+        trials$day_table <- day_table
+        
+        # Reorder columns in data frame
+        trials <- trials %>%
+          dplyr::select(subj_id, date, time, box, day_table, everything())
+        
+        # Bind to main data
+        data <- rbind(data, trials)
+      }# end of subject appears in subject_in_session
+    } # end of subject loop
+  }#end of 'subject appears' FILE if
+} # end of FILE loop
+
+prl_n_8 <- data %>%
+  # Use this filter if you want to narrow down your data set here
+  # filter(subj_id %in% c(3, 4),
+  #        date %in% c('23-Apr-2025', '24-Apr-2025')) %>%
+  # Create and rename variables from the sessions
+  mutate(reward = c47,
+         chose_left = c38 + c43,
+         trial = c1 + 1,
+         reward_latency = (c46 - c45) * c47,
+         response_latency = (c36 - c35) + (c41-c40),
+         initiate_latency = c16 - c15,
+         left_is_active = as.numeric(c35 > c40)
+  ) %>%
+  # Select the columns to carry forward - we drop all the columns
+  # with raw data from K-Limbic at this point
+  dplyr::select(subj_id, date, time, box, day_table,
+                trial, chose_left, reward, left_is_active,
+                reward_latency, response_latency, initiate_latency)
+
+# Save the data with a useful name
+write_csv(prl_n_8,
+          file = paste0('Processed Data/','PRL_PRELIM.csv'))
+
---
+++ Update_Processed.R
@@ -0,0 +1,136 @@
+###Process for reversals, accuracy, win-stay & lose-shift
+
+data <- read_csv("Processed Data/PRL_PRELIM.csv" )
+
+prl<-(data|>
+         filter(
+           day_table=="PRL - Constant Criteria")
+       
+)
+
+prl$date<-dmy(prl$date)
+prl<-prl[complete.cases(prl), ]
+prl$opt<-0
+prl$towards_criterion<-0
+prl$win_stay<-0
+prl$lose_shift<-0
+prl$true_win_stay<-0
+prl$perseverence_counter<-0
+prl$perseverence<-0
+
+for (trial in 1:dim(prl)[1]){
+  #first check if optimum side was chosen
+  if (prl$chose_left[trial]==prl$left_is_active[trial]){
+    prl$opt[trial]<-1
+  }
+  
+  #CRITERION CALCULATION:
+  #if its the first trial then towards criterion is set to 0 if
+  #false start or 1 if optimum choice was made
+  
+  if (prl$trial[trial]==1){
+    prl$towards_criterion[trial]<-prl$opt[trial]
+    
+    #if it is not the first trial, check if previous trial was criteria
+  } else if (prl$towards_criterion[trial-1]==6){
+    #if it was restart as before 
+    prl$towards_criterion[trial]<-prl$opt[trial]
+    #if its not then we check whether the trial was correct
+  } else if (prl$opt[trial]==1){
+    #if so add to the counter
+    prl$towards_criterion[trial]<-prl$towards_criterion[trial-1]+1
+    #if not the counter resets to 0
+  } else if (prl$opt[trial]==0){
+    prl$towards_criterion[trial]<-0
+  }
+  
+  #WINSTAY vs LOSESHIFT
+  
+  if (!prl$trial[trial]==1){
+    #first check if they won the previous trial
+    if (prl$reward[trial-1]==1){
+      #yes they got a reward, a WIN. Did they stick this time?
+      if (prl$chose_left[trial-1]==prl$chose_left[trial]){
+        #yes they stuck!
+        prl$win_stay[trial]<-1
+      } #no need to do anything if they switched, automatic is false
+    } else if (prl$reward[trial-1]==0) {
+      #last time they lost, no reward ):, did they shift their strategy this time?
+      if (!prl$chose_left[trial-1]==prl$chose_left[trial]){
+        #yes, they chose differently this time!
+        prl$lose_shift[trial]<-1
+      } #end they shifted
+    } #end no reward
+    
+    #TRUE WIN_STAY
+    #did they chose the correct side last time?
+    if (prl$opt[trial-1]==1){
+      #yes! DId they stick (regardless of feedback)
+      if (prl$chose_left[trial-1]==prl$chose_left[trial]){
+        #yes!
+        prl$true_win_stay[trial]<-1
+      } #end of stuck
+    } #end of chose optimum side
+  } #end not the first trial, winstay loseshift
+  
+  #PERSEVERENCE
+  #not the first trial
+  if (!prl$trial[trial]==1){
+    if (prl$towards_criterion[trial-1]==6){
+      #reversal has just happened. do they chose a different side?
+      if (prl$chose_left[trial-1]==prl$chose_left[trial]){
+        #NO, add one to counter
+        prl$perseverence_counter[trial]<-1
+      }
+    }
+    #previously the mouse was persevering
+    if (prl$perseverence_counter[trial-1]>0){
+      #this time did they persevere?
+      if (prl$chose_left[trial-1]==prl$chose_left[trial]){
+        #yes, increase counter
+        prl$perseverence_counter[trial]<-prl$perseverence_counter[trial-1]+1
+        }
+    }
+    
+    if (prl$perseverence_counter[trial]==0){
+      if (prl$perseverence_counter[trial-1]>0){
+        prl$perseverence[trial-1]<-prl$perseverence_counter[trial-1]
+      }
+    }
+  }
+}#end whole thing
+
+prl$perseverence_true<-prl$perseverence>0
+
+prl_sum<-prl|>
+  group_by(subj_id,date)|>
+  summarise(crits=sum(towards_criterion==6),
+            trials=max(trial),
+            crit_per_100=((sum(towards_criterion==6))/max(trial))*100,
+            win_stay_prob=(sum(win_stay==1))/sum(reward==1),
+            lose_shift_prob=sum(lose_shift==1)/sum(reward==0),
+            percent_chose_left=(sum(chose_left==1)/max(trial))*100,
+            accuracy=(sum(opt)/max(trial))*100,#
+            n_trials=max(trial),
+            true_win_stay_prob=(sum(true_win_stay==1))/sum(opt==1),
+            m_perseverence=sum(perseverence)/sum(perseverence_true)
+            )
+
+m_prl_sum<-prl_sum|>
+  group_by(date)|>
+  summarise(m_crit_per_100=mean(crit_per_100),
+            m_win_stay_prob=mean(win_stay_prob),
+            m_lose_shift_prob=mean(lose_shift_prob),
+            m_accuracy=mean(accuracy),
+            m_crits=mean(crits),
+            m_n_trials=mean(n_trials),
+            m_true_win_stay_prob=mean(true_win_stay_prob),
+            m_m_perseverence=mean(m_perseverence,na.rm=TRUE))
+
+write_csv(prl,
+          file = paste0('Processed Data/','PRL_PROCESSED.csv'))
+write_csv(prl_sum,
+          file = paste0('Processed Data/','PRL_SUM.csv'))
+write_csv(m_prl_sum,
+          file = paste0('Processed Data/','PRL_SUM_MEANS.csv'))
+
